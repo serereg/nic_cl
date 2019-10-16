150db74e54562761d8b41560125444f9b0227c61
serereg <56562368+serereg@users.noreply.github.com>
2019-10-16 08:39:50 +0300

    Add files via upload

---
+++ Cooler.py
@@ -0,0 +1,42 @@
+
+class Parameter:
+    Value = 0
+    Fault = False
+    TagName = "OPC.Item"
+    def __init__(self, name):
+        self.TagName = name
+        #print(self.TagName)
+    
+
+class Cooler:
+    """Управление охладителем"""
+    num = 1 # cooler num
+    name = "CKT" # cooler name
+    SP = 0
+    PV = Parameter('') # Parameter for control
+    TagControl = ''
+    TagSP = ''
+    ControlString = ""
+    def __init__(self, num):
+        self.num = num
+        name = "CKT" + num
+        self.PV = Parameter(name)
+        self.TagControl = 'OPC.' + name + '.Control'
+        self.TagSP = 'OPC.' + name + '.SP'
+        pass
+    
+    def YOn(self):
+        pass
+    def YOff(self):
+        pass
+    def SetSP(self):
+        pass
+    def GetPV(self):
+        return self.PV.Value
+        
+
+def main():
+    CKT1 = Cooler('1')
+    print(CKT1.PV.Value)
+
+main()
